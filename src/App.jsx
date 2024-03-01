45a8e927ac615947a07e76fbad4a2357c734606e
KharlAbban <khvngkharl123@gmail.com>
2024-03-01 15:00:10 +0000

    Initial setup for tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-4xl text-center text-blue-800 underline hover:text-stone-700">
+      Hello to the f**kn world
+    </div>
+  )
+}
+
+export default App;
